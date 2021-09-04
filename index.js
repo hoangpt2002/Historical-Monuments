9e2cc270da8f2b149b1e973c6bb7fb790ca2322d
hoangpt2002 <84027762+hoangpt2002@users.noreply.github.com>
2021-09-04 10:26:45 +0700

    create

---
+++ index.js
@@ -0,0 +1,21 @@
+var express = require("express");
+var app = express();
+var port = process.env.PORT || 3000;
+app.listen(port,function (){
+    console.log("Server is running");
+});
+app.use(express.static("public"));
+app.set("view engine","ejs");
+
+app.get("/",function (req,res) {
+    res.render("home");
+})
+app.get("/home",function (req,res) {
+    res.render("home")
+})
+app.get("/historical",function (req,res) {
+    res.render("historical");
+})
+app.get("/historical/geographical", function (req,res) {
+    res.render("geographical");
+})
