16529e5ba9c23e3faf12c76135c94f46b9d3e782
hoangpt2002 <84027762+hoangpt2002@users.noreply.github.com>
2021-09-29 10:33:51 +0700

    finally

--- index.js
+++ index.js
@@ -1,21 +1,125 @@
 var express = require("express");
+const mongoose = require('mongoose');
 var app = express();
 var port = process.env.PORT || 3000;
-app.listen(port,function (){
+app.listen(port,() => {
     console.log("Server is running");
 });
 app.use(express.static("public"));
 app.set("view engine","ejs");
 
-app.get("/",function (req,res) {
-    res.render("home");
+mongoose.connect(
+            'mongodb+srv://hoangnv02:0wZ3wCuUeO7HhXTo@cluster0.17irg.mongodb.net/Historical?retryWrites=true&w=majority',
+            {
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+            }
+        )
+        .then(() => console.log('Database connected!'))
+        .catch(err => console.log(err));
+
+const dataSchema = {
+    ID: Number,
+    HinhAnh: String,
+    TieuDe: String,
+    NoiDung: String,
+    Slug: String,
+    TacGia: String,
+    Ngay: String,
+    Thang : String,
+    Loai: String,
+}
+
+const Data = mongoose.model("data", dataSchema);
+
+app.get("/",(req,res,next) => {
+    Data.find({})
+        .then((data) => {
+            res.render("home", {
+                data: data
+            })
+        })
+        .catch(next);
 })
-app.get("/home",function (req,res) {
-    res.render("home")
+
+app.get("/historical/geographical/:id", (req,res,next) => {
+    Data.find({Loai: "Geographical"})
+        .then((data) => {
+            res.render("geographical-news", {
+                data: data,
+                id: req.params.id
+            })
+        })
+        .catch(next);
 })
-app.get("/historical",function (req,res) {
-    res.render("historical");
+
+app.get("/historical/geographical", (req,res, next) => {
+    Data.find({Loai: "Geographical"})
+        .then((data) => {
+            res.render("geographical", {
+                data: data
+            })
+        })
+        .catch(next);
 })
-app.get("/historical/geographical", function (req,res) {
-    res.render("geographical");
+
+app.get("/historical/time/:id", (req,res,next) => {
+    Data.find({Loai: "Time"})
+        .then((data) => {
+            res.render("time-news", {
+                data: data,
+                id: req.params.id
+            })
+        })
+        .catch(next);
 })
+
+app.get("/historical/time", (req,res,next) => {
+    Data.find({Loai: "Time"})
+        .then((data) => {
+            res.render("time", {
+                data: data
+            })
+        })
+        .catch(next);
+})
+
+app.get("/historical/customs/:id", (req,res,next) => {
+    Data.find({Loai: "Customs"})
+        .then((data) => {
+            res.render("customs-news", {
+                data: data,
+                id: req.params.id
+            })
+        })
+        .catch(next);
+})
+
+app.get("/historical/customs", (req,res,next) => {
+    Data.find({Loai: "Customs"})
+        .then((data) => {
+            res.render("customs", {
+                data: data
+            })
+        })
+        .catch(next);
+})
+
+app.get("/gallery",(req,res, next) => {
+    Data.find({})
+        .then((data) => {
+            res.render("gallery", {
+                data: data
+            })
+        })
+        .catch(next);
+})
+
+app.get("/contact",(req,res) => {
+    res.render("contact");
+})
+
+app.post("/contact", (req,res) => {
+    res.render("contact-success")
+})
+
